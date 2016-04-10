f8a9fdaa3fcb22b44e26f723c90d6c75d02eec64
Ran Elkobi <ran@Simplee.com>
2016-04-10 16:33:39 +0300

    First commit and README update

---
+++ README.md
@@ -0,0 +1,7 @@
+## New Options Project
+
+Used the following options in "rails new":
+
+1. -T : No Test files 
+2. -J : Skipping JavaScripts files
+3. -f : overiiten existing files
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewOptions::Application.config.secret_token = 'f7841233003c7c2d6f62fa1599eaaf67cbf956e20b9dedde2372f94b1bbda2e0a5762c28d79d35bf85a0a546c0eea4989a54ddfac6053513846479c639334cd5'
